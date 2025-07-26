a51f175744a88d73a7f81f64b68e82ada4c4bf5a
Tanmax Sharma <tanmaysharma.eng05@gmail.com>
2025-07-26 11:52:18 +0530

    Header-Changed_video_Update-removed-more-grid

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -75,28 +75,28 @@ const portfolioItems = [
     video: VIDEO_SOURCES.bear,
     poster: POSTER.manager,
   },
-  {
-    name: "Motion Graphics Artist",
-    video: VIDEO_SOURCES.sample5s,
-    poster: POSTER.motion,
-  },
-  {
-    name: "Copywriter",
-    video: VIDEO_SOURCES.sample10s,
-    poster: POSTER.copywriter,
-  },
-  {
-    name: "Creative Director",
-    video: VIDEO_SOURCES.ocean,
-    poster: POSTER.director,
-  },
-  { name: "UI/UX Designer", video: VIDEO_SOURCES.city, poster: POSTER.ui },
-  {
-    name: "Photographer",
-    video: VIDEO_SOURCES.forest,
-    poster: POSTER.photographer,
-  },
-  { name: "CGI/VFX Artist", video: VIDEO_SOURCES.night, poster: POSTER.vfx },
+  // {
+  //   name: "Motion Graphics Artist",
+  //   video: VIDEO_SOURCES.sample5s,
+  //   poster: POSTER.motion,
+  // },
+  // {
+  //   name: "Copywriter",
+  //   video: VIDEO_SOURCES.sample10s,
+  //   poster: POSTER.copywriter,
+  // },
+  // {
+  //   name: "Creative Director",
+  //   video: VIDEO_SOURCES.ocean,
+  //   poster: POSTER.director,
+  // },
+  // { name: "UI/UX Designer", video: VIDEO_SOURCES.city, poster: POSTER.ui },
+  // {
+  //   name: "Photographer",
+  //   video: VIDEO_SOURCES.forest,
+  //   poster: POSTER.photographer,
+  // },
+  // { name: "CGI/VFX Artist", video: VIDEO_SOURCES.night, poster: POSTER.vfx },
 ];
 
 
